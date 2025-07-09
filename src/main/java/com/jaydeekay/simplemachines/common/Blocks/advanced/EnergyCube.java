65eff4465020c005efdf9a1ff019517699dcc141
julus54321 <julus.contact@gmail.com>
2025-07-09 13:05:01 +0200

    setup for menu

--- src/main/java/com/jaydeekay/simplemachines/common/Blocks/advanced/EnergyCube.java
+++ src/main/java/com/jaydeekay/simplemachines/common/Blocks/advanced/EnergyCube.java
@@ -1,11 +1,17 @@
 package com.jaydeekay.simplemachines.common.Blocks.advanced;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.NotNull;
 
 public class EnergyCube extends Block implements EntityBlock {
@@ -18,4 +24,16 @@ public class EnergyCube extends Block implements EntityBlock {
     public BlockEntity newBlockEntity(final @NotNull BlockPos pos, final @NotNull BlockState state) {
         return new EnergyCubeEntity(pos, state);
     }
+
+    public @NotNull InteractionResult use(@NotNull BlockState state, @NotNull Level level, @NotNull BlockPos pos,
+                                          @NotNull Player player, @NotNull InteractionHand hand, @NotNull BlockHitResult hit) {
+        if (!level.isClientSide()) {
+            BlockEntity be = level.getBlockEntity(pos);
+            if (be instanceof MenuProvider provider) {
+                player.openMenu(provider);
+            }
+        }
+        return InteractionResult.SUCCESS;
+    }
+
 }
